c68e5d684eec61511be3c3805666f06207fa9da6
Shota MITSUI <r.627sh@gmail.com>
2010-08-16 00:26:57 +0900

    modified:application/modules/admin/controller/IndexController.php

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -5,5 +5,20 @@ class Admin_IndexController extends Zend_Controller_Action
     public function indexAction()
     {
     }
+    
+    public function updateAmbitionAction()
+    {
+    	$this->_redirect('/admin/index/index');
+    }
+
+    public function formGoalAction()
+    {
+    }
+
+    public function updateGoalAction()
+    {
+    	$this->_redirect('/admin/index/form-goal');
+    }
+
 }
 
