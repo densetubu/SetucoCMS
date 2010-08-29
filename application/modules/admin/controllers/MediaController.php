064eba5a83e454069d48e5e8264cc59c6fc6c875
akitsukada <tsukada.akihiro@gmail.com>
2010-08-29 09:42:47 +0900

    pageとmedia/formのスケルトン・phtml。(#142,#149,#189)
    Mediaサービスのクラス追加。

--- application/modules/admin/controllers/MediaController.php
+++ application/modules/admin/controllers/MediaController.php
@@ -1,42 +1,61 @@
 <?php
 /**
- * 管理側のアップロードしたファイルを管理するのコントローラーです。
+ * ファイル管理のコントローラ
  *
  * LICENSE: ライセンスに関する情報
  *
- * @category 	Setuco
- * @package 	Admin
- * @subpackage  Controller
- * @copyright   Copyright (c) 2010 SetucoCMS Project.
- * @license
- * @version
- * @link
- * @since       File available since Release 0.1.0
- * @author     
+ * @category   Setuco
+ * @package    Admin
+ * @subpackage Controller
+ * @license    http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
+ * @copyright  Copyright (c) 2010 SetucoCMS Project.(http://sourceforge.jp/projects/setucocms)
+ * @link       
+ * @version    
+ * @since      File available since Release 0.1.0
+ * @author     akitsukada
  */
 
 
 /**
- * @category    Setuco
- * @package     Admin
- * @subpackage  Controller
- * @copyright   Copyright (c) 2010 SetucoCMS Project.
- * @license
- * @author 
+ * ファイル管理画面の操作を行うコントローラ
+ * 
+ * @package    Admin
+ * @subpackage Controller
+ * @author     akitsukada
  */
 class Admin_MediaController extends Setuco_Controller_Action_Admin
 {
     /** 
      *
-     * ファイルのアップロードフォーム
+     * ファイルのアップロードフォームや
      * アップロードしてあるファイルの一覧を表示するアクションです
      *
      * @return void
-     * @author 
+     * @author akitsukada
      * @todo 内容の実装 現在はスケルトン
      */
     public function indexAction()
     {
+
+        /* リクエスト取得 */
+        $req = $this->getRequest();
+
+        /* ファイルのデータ取得 */
+        // 絞込条件の有無
+        $fileType = $req->getUserParam('fileType');
+        $media = new Admin_Model_Media();
+        if ($fileType !== null) {
+
+        } else {
+        }
+         
+
+        // paginator 設定
+        $currentPage = 1;
+        if ($req->getUserParam('page') !== null && is_numeric($req->getUserParam('page'))) {
+            $currentPage = $req->getUserParam('page');
+        }
+        echo $currentPage;
 
     }
 
@@ -45,7 +64,7 @@ class Admin_MediaController extends Setuco_Controller_Action_Admin
      * indexアクションに遷移します
      *
      * @return void
-     * @author 
+     * @author akitsukada
      * @todo 内容の実装 現在はスケルトン
      */
     public function createAction()
@@ -58,7 +77,7 @@ class Admin_MediaController extends Setuco_Controller_Action_Admin
      * indexアクションに遷移します
      *
      * @return void
-     * @author 
+     * @author akitsukada
      * @todo 内容の実装 現在はスケルトン
      */
     public function updateAction()
@@ -71,7 +90,7 @@ class Admin_MediaController extends Setuco_Controller_Action_Admin
      * indexアクションに遷移します
      *
      * @return void
-     * @author 
+     * @author akitsukada
      * @todo 内容の実装 現在はスケルトン
      */
     public function deleteAction()
@@ -79,4 +98,15 @@ class Admin_MediaController extends Setuco_Controller_Action_Admin
         $this->_redirect('/admin/media/index');        
     }
 
+    /** 
+     * アップロード済みのファイルを編集するアクションです
+     *
+     * @return void
+     * @author akitsukada
+     * @todo 内容の実装 現在はスケルトン
+     */
+    public function formAction()
+    {
+
+    }
 }
---
+++ application/modules/admin/models/Media.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 管理側のファイル管理用サービス
+ *
+ * LICENSE: ライセンスに関する情報
+ *
+ * @category   Setuco
+ * @package    Admin
+ * @subpackage Model
+ * @license    http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
+ * @copyright  Copyright (c) 2010 SetucoCMS Project.(http://sourceforge.jp/projects/setucocms)
+ * @link       
+ * @version    
+ * @since      File available since Release 0.1.0
+ * @author     akitsukada
+ */
+
+/**
+ * ファイル管理クラス
+ * 
+ * @package    Admin
+ * @subpackage Model
+ * @author     akitsukada
+ */
+class Admin_Model_Media
+{
+
+}
