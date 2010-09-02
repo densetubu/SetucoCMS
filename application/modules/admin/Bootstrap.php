c42220d6d2ced27b22109f11808db7ef81a4a6ea
suzuki masayuki <supasu145@gmail.com>
2010-09-03 00:44:35 +0900

    エラー時に、defaultモジュールでもadminのレイアウトが表示されてしまっていた問題の対応 defaultモジュールのナビゲーションのビューファイルにliタグが閉じていなかったのを修正

--- application/modules/admin/Bootstrap.php
+++ application/modules/admin/Bootstrap.php
@@ -1,11 +1,5 @@
 <?php
 class Admin_Bootstrap extends Zend_Application_Module_Bootstrap
 {
-    protected function _initLayout()
-    {
-        $options = array('layout'     => 'layout',
-                         'layoutPath' => APPLICATION_PATH . '/modules/admin/views/layouts',
-                         'content'    => 'content');
-        Zend_Layout::startMvc($options);
-    }
+
 }
