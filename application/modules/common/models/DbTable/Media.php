4fb80c226f8f777d5ccd4ca645a4ed432723eff8
Shota MITSUI <r.627sh@gmail.com>
2010-09-04 21:58:14 +0900

    DAOクラスを修正しました。
    phpタグの閉じ括弧の削除、コメント記述

--- application/modules/common/models/DbTable/Media.php
+++ application/modules/common/models/DbTable/Media.php
@@ -1,7 +1,17 @@
 <?php
+/**
+ * MediaテーブルのDAOクラスです。
+ *
+ * @category   Setuco
+ * @package    Common_Model
+ * @subpackage DbTable
+ * @copyright  Copyright (c) 2010 SetucoCMS Project.
+ * @since      File available since Release 0.1.0
+ * @author     mitchang
+ */
 class Common_Model_DbTable_Media extends Zend_Db_Table_Abstract
 {
 	protected $_name = 'media';
 	protected $_primary = 'id';
-?>
+}
 
--- application/modules/common/models/DbTable/Page.php
+++ application/modules/common/models/DbTable/Page.php
@@ -1,8 +1,17 @@
 <?php
+/**
+ * PageテーブルのDAOクラスです。
+ *
+ * @category   Setuco
+ * @package    Common_Model
+ * @subpackage DbTable
+ * @copyright  Copyright (c) 2010 SetucoCMS Project.
+ * @since      File available since Release 0.1.0
+ * @author     mitchang
+ */
 class Common_Model_DbTable_Page extends Zend_Db_Table_Abstract
 {
 	protected $_name = 'page';
 	protected $_primary = 'id';
 }
-?>
 
