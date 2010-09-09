0691e0fe8b823f934836d2c63a149124f69782f1
Mizuki Yamanaka <charles.vineyard@gmail.com>
2010-09-09 16:15:20 +0900

    コメント修正

--- library/Setuco/Form.php
+++ library/Setuco/Form.php
@@ -1,8 +1,25 @@
 <?php
 /**
- * SetucoCMS用に拡張したZend_Form
- * 
- * @author Yuu Yamanaka
+ * SetucoCMS 用に拡張した Zend_Form です。
+ *
+ * LICENSE: ライセンスに関する情報
+ *
+ * @category    Setuco
+ * @package     Form
+ * @copyright   Copyright (c) 2010 SetucoCMS Project.
+ * @license
+ * @version
+ * @link
+ * @since       File available since Release 0.1.0
+ * @author      Yuu Yamanaka
+ */
+
+/**
+ * @category    Setuco
+ * @package     Form
+ * @copyright   Copyright (c) 2010 SetucoCMS Project.
+ * @license
+ * @author      Yuu Yamanaka
  */
 class Setuco_Form extends Zend_Form
 {
@@ -11,6 +28,7 @@ class Setuco_Form extends Zend_Form
      * 
      * @param mixed $elements 要素名か要素名の配列
      * @return void
+     * @author Yuu Yamanaka
      */
     public function setMinimalDecoratorElements($elements)
     {
@@ -20,9 +38,11 @@ class Setuco_Form extends Zend_Form
     /**
      * Dojoエレメントを有効にする
      * 
-     * @return Setuco_Form
+     * @return void
+     * @author Yuu Yamanaka
      */
     public function enableDojo() {
         Zend_Dojo::enableForm($this);
     }
+
 }
