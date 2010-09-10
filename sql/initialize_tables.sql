2aa4dcc0af160e0266a675b6a9a2018c5370499d
akitsukada <tsukada.akihiro@gmail.com>
2010-09-10 20:38:35 +0900

    SQLスクリプト、初期値登録のtimestampをnow()ではなく適当な日付に訂正

--- sql/initialize_tables.sql
+++ sql/initialize_tables.sql
@@ -2,14 +2,14 @@
 * CREATE TABLES
 ******************************************************/
 
--- タグ表 --
+-- タグ表 
 CREATE TABLE tag (
     id      INT AUTO_INCREMENT NOT NULL,
     name    VARCHAR(255) UNIQUE NOT NULL,
     PRIMARY KEY(id)
 );
 
--- アカウント表 --
+-- アカウント表 
 CREATE TABLE account (
     id       INT AUTO_INCREMENT NOT NULL,
     login_id VARCHAR(255) UNIQUE NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE account (
     PRIMARY KEY(id)
 );
 
--- メディア表 --
+-- メディア表 
 CREATE TABLE media (
     id          INT AUTO_INCREMENT NOT NULL,
     name        TEXT NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE media (
     PRIMARY KEY(id)
 );
 
--- カテゴリ表 --
+-- カテゴリ表 
 CREATE TABLE category (
     id          INT AUTO_INCREMENT NOT NULL,
     name        VARCHAR(255) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE category (
     FOREIGN KEY(parent_id) REFERENCES category(id) ON DELETE CASCADE
 ) TYPE=INNODB;
 
--- ページ表 --
+-- ページ表 
 CREATE TABLE page (
     id          INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title       TEXT NOT NULL,
@@ -52,21 +52,21 @@ CREATE TABLE page (
     update_date TIMESTAMP NOT NULL
 ) TYPE=INNODB;
 
--- ページメディア表 --
+-- ページメディア表 
 CREATE TABLE page_media (
     page_id     INT NOT NULL REFERENCES page(id) ON DELETE CASCADE,
     media_id    INT NOT NULL REFERENCES media(id) ON DELETE CASCADE,
     PRIMARY KEY(media_id,page_id)
 ) TYPE=INNODB;
 
--- ページタグ表 --
+-- ページタグ表 
 CREATE TABLE page_tag (
     page_id     INT NOT NULL REFERENCES page(id) ON DELETE CASCADE,
     tag_id      INT NOT NULL REFERENCES tag(id) ON DELETE CASCADE,
     PRIMARY KEY(page_id,tag_id)
 ) TYPE=INNODB;
 
--- サイト表 --
+-- サイト表 
 CREATE TABLE site (
     id          INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name        TEXT NOT NULL,
@@ -76,13 +76,13 @@ CREATE TABLE site (
     open_date   TIMESTAMP NOT NULL
 );
 
--- 野望表 --
+-- 野望表 
 CREATE TABLE ambition (
     id          INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     ambition    TEXT NOT NULL
 );
 
--- 目標表 --
+-- 更新目標表 
 CREATE TABLE goal (
     id           INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     page_count   INT NOT NULL,
@@ -103,20 +103,25 @@ ON page_tag(tag_id);
 * INSERT DEFAULTS
 ******************************************************/
 
--- サイト表へ初期値の追加 --
+-- 初期値の登録
 INSERT INTO site 
     (name, url, comment, keyword, open_date)
 VALUES
-    ('日本電子専門学校 電設部', 'http://penguin.jec.ac.jp/', '日本電子専門学校電設部SetucoCMSプロジェクトです。', 'せつこ,俺だ,結婚,してくれ', now());
+    ('日本電子専門学校 電設部', 'http://penguin.jec.ac.jp/', '日本電子専門学校電設部SetucoCMSプロジェクトです。', 'せつこ,俺だ,結婚,してくれ', '2010-02-11 10:00:00');
 
--- 野望表へ初期値の追加 --
 INSERT INTO ambition 
     (ambition)
 VALUES
     ('せつこーおれだーけっこｎ（ｒｙ');
 
--- 目標表へ初期値の追加 --
 INSERT INTO goal 
     (page_count, target_month)
 VALUES
-    (13, now());
+    (13, '2010-02-01 00:00:00'),
+    (15, '2010-03-01 00:00:00'),
+    (10, '2010-04-01 00:00:00'),
+    (14, '2010-05-01 00:00:00'),
+    (14, '2010-06-01 00:00:00'),
+    (14, '2010-07-01 00:00:00'),
+    (14, '2010-08-01 00:00:00'),
+    (17, '2010-09-01 00:00:00');
