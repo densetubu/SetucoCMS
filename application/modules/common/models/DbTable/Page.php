c1c1ce03cdf8b27b9ba8de0448917c8b25f008b8
akitsukada <tsukada.akihiro@gmail.com>
2010-10-14 02:58:21 +0900

    common/models/Dbtable/Page.phpのインデント調整

--- application/modules/common/models/DbTable/Page.php
+++ application/modules/common/models/DbTable/Page.php
@@ -22,52 +22,90 @@
  */
 class Common_Model_DbTable_Page extends Zend_Db_Table_Abstract
 {
-	/**
-	 * テーブル名
-	 *
-	 * @var string
-	 */
-	protected $_name = 'page';
 
-	/**
-	 * プライマリーキーのカラム名
-	 *
-	 * @var string
-	 */
-	protected $_primary = 'id';
+    /**
+     * テーブル名
+     *
+     * @var string
+     */
+    protected $_name = 'page';
+    /**
+     * プライマリーキーのカラム名
+     *
+     * @var string
+     */
+    protected $_primary = 'id';
 
-	/**
-	 * 記事の状態　公開
-	 */
-	const STATUS_OPEN = 1;
+    /**
+     * 記事の状態　公開
+     */
+    const STATUS_OPEN = 1;
 
-	/**
-	 * 記事の状態　下書き
-	 */
-	const STATUS_DRAFT = 0;
+    /**
+     * 記事の状態　下書き
+     */
+    const STATUS_DRAFT = 0;
 
-	/**
-	 * 新着記事を取得する
-	 * 
-	 * @param int $getPageCount 何件の記事を取得するのか
-	 * @author suzuki-mar
-	 */
-	public function findNewPages($getPageCount)
-	{
-		$select = $this->select();
+    /**
+     * 新着記事を取得する
+     * 
+     * @param int $limitGetNewPage 何件の記事を取得するのか
+     * @author suzuki-mar
+     */
+    public function findNewPages($limitGetNewPage)
+    {
+        $select = $this->select();
 
-		//公開しているものしか取得しない
-		$select->where('status = ?', self::STATUS_OPEN);
-		
-		//編集日時が最新順にソートする
-		$select->order('update_date DESC');
-		
-		//指定した件数しか取得しない
-		$select->limit($getPageCount);
-		
-		$result = $this->fetchAll($select)->toArray();
-		return $result;
-	}
-	
+        //公開しているものしか取得しない
+        $select->where('status = ?', self::STATUS_OPEN);
+
+        //編集日時が最新順にソートする
+        $select->order('update_date DESC');
+
+        //指定した件数しか取得しない
+        $select->limit($limitGetNewPage);
+
+        $result = $this->fetchAll($select)->toArray();
+        return $result;
+    }
+
+    /**
+     * カテゴリを指定して記事を取得する
+     *
+     * @param int $catId 取得したいカテゴリのID
+     * @param int $currentPage ページネータの何ページ目を表示するか
+     * @param int $limig １ページに表示する記事数
+     * @author akitsukada
+     * @return
+     */
+    public function findPagesByCategoryId($catId, $currentPage = null, $limit = null)
+    {
+        $select = $this->select();
+
+        $select->from(array('p' => $this->_name));
+
+        //公開している記事のみ取得する
+        $select->where('status = ?', self::STATUS_OPEN);
+
+        //指定されたカテゴリの記事のみ取得する
+        $select->where('category_id = ?', $catId);
+
+        //編集日時の降順にソートする
+        $select->order('update_date DESC');
+
+        //投稿者名取得のためJOIN
+        $select->joinLeft(array('a' => 'account'), 'a.id = p.account_id', array('account_name' => 'a.nickname'));
+        $select->setIntegrityCheck(false);
+
+        if (!is_null($currentPage) && !is_null($limit)) {
+            //ページネータの設定（何ページ目を表示するか、何件ずつ表示するか）
+            $select->limitPage($currentPage, $limit);
+        }
+        
+        $result = $this->fetchAll($select)->toArray();
+
+        return $result;
+        
+    }
+
 }
-
