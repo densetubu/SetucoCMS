9213c57d4da2828db14a01b0380999af463fbc87
charlesvineyard <charles.vineyard@gmail.com>
2010-10-25 17:14:34 +0900

    サイト開設日と経過日数の取得のサービスを実装
    関連チケット #197

--- application/modules/admin/models/Site.php
+++ application/modules/admin/models/Site.php
@@ -68,7 +68,7 @@ class Admin_Model_Site
      *
      * 最終更新日はページの公開日時が最新のものです。
      *
-     * @return array 最終更新日と経過日数の配列
+     * @return array 最終更新日(lastUpdateDate Zend_Date)と経過日数(pastDays int)の配列
      * @author charlesvineyard
      */
     public function getLastUpdateDateWithPastDays()
@@ -82,15 +82,35 @@ class Admin_Model_Site
     /**
      * サイト開設日とその日からの経過日数を取得します。
      *
-     * @return array サイト開設日と経過日数の配列
+     * @return array サイト開設日(openDate Zend_Date)と経過日数(pastDays int)の配列
      * @author charlesvineyard
      */
     public function getOpenDateWithPastDays()
     {
+        $site = $this->getSiteInfo();
         $openDate = new Zend_Date();
-        $openDate->setDate('2009-09-01', 'YYYY-MM-dd', 'ja_JP');
+        $openDate->setDate($site['open_date'], 'YYYY-MM-dd', 'ja_JP');
         return array('openDate' => $openDate,
-                     'pastDays' => 300);
+                     'pastDays' => $this->_findPastDays($openDate, new Zend_Date()));
+    }
+    
+    /**
+     * ある日付から他の日付までの経過日数を求めます。
+     * 
+     * 引数の日付のHOUR以下の設定は切り捨てて計算します。
+     * $toDateが$fromDateより小さい場合はマイナス値が返ります。
+     * 
+     * @param  Zend_Date $fromDate 経過日数の起算日
+     * @param  Zend_Date $toDate   経過日数の終算日
+     * @return int 経過日数
+     * @author charlesvineyard
+     */
+    private function _findPastDays($fromDate, $toDate)
+    {
+        $fromDate->setTime('00:00:00', 'HH:mm:ss', 'ja_JP');
+        $toDate->setTime('00:00:00', 'HH:mm:ss', 'ja_JP');
+        $pastDaysValue = $toDate->toValue() - $fromDate->toValue();
+        return (int)($pastDaysValue / 60 / 60 / 24);
     }
 
 }
