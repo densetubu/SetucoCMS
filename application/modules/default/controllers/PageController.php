fccd87165c4be0ee06cc186f7e89cbcfadd4f3a4
akitsukada <tsukada.akihiro@gmail.com>
2010-10-26 00:53:37 +0900

    管理側Media周りのバグプチ修正

--- application/modules/default/controllers/PageController.php
+++ application/modules/default/controllers/PageController.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * 閲覧側のページを表示するコントローラーです。
  *
@@ -16,8 +15,6 @@
  * @author      suzuki-mar akitsukada
  */
 
-
-
 /**
  * @category    Setuco
  * @package     Default
@@ -26,18 +23,34 @@
  */
 class PageController extends Setuco_Controller_Action_DefaultAbstract
 {
+    /**
+     * 新着記事表示用に標準で何件取得するか
+     */
+    const LIMIT_GET_NEW_PAGE = 10;
 
-    /** 
+    /**
+     * カテゴリ別検索で標準で何件取得するか
+     */
+    const LIMIT_GET_PAGE_BY_CATEGORY = 5;
+
+
+    /**
+     * サービスクラス
+     */
+    private $_service = null;
+
+    /**
      * アクションの共通設定
      *
      * @return void
      * @author suzuki_mar
      */
     public function init()
-    {   
+    {
         //モジュール間の共通の設定を実行
         parent::init();
 
+        $this->_service = new Default_Model_Page;
     }
 
     /**
@@ -61,8 +74,7 @@ class PageController extends Setuco_Controller_Action_DefaultAbstract
      */
     public function searchAction()
     {
-        
-        
+
     }
 
     /**
@@ -70,13 +82,33 @@ class PageController extends Setuco_Controller_Action_DefaultAbstract
      *
      * @return void
      * @author akitsukada
-     * @todo 実装（現在スケルトン）
+     * @todo 一覧にはコンテンツの頭５行のみ表示する（現在とりあえず生コンテンツデータそのまま表示）
      */
     public function categoryAction()
     {
 
-    }
+        $id = $this->_getParam('id');
+        if (!is_numeric($id) && !is_null($id) || is_numeric($id) && $id < 0) {
+            // 不正なIDが指定されたら閲覧側トップにリダイレクト
+            $this->_helper->redirector('index', 'index');
+        } elseif ($id == 0) {
+            $id = null;
+        }
 
+        $currentPage = $this->_getParam('page', 1);
+        if (!is_numeric($currentPage)) {
+            $currentPage = 1;
+        }
+
+        $categoryService = new Default_Model_Category();
+
+        $this->view->entries = $this->_service->getPagesByCategory($id, $currentPage);
+        $this->view->category = $categoryService->getCategoryById($id);
+
+        // ページネーター用の設定
+        $this->view->currentPage = $currentPage;
+        $this->setPagerForView($this->_service->countPagesByCategory($id), 5);
+    }
 
     /**
      * あるタグがつけられたページの一覧を表示する
