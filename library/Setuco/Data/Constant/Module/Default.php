e553cce2be0c59ca8f6e2dd4d435da91830ae80a
suzuki_mar <supasu145@gmail.com>
2010-11-21 15:15:52 +0900

    addし忘れ
    忘れたもの
    管理者側のアクションメッセージのパーシャル
    デフォルトモジュールの定数ホルダー
    開発用のデータのSQL

---
+++ library/Setuco/Data/Constant/Module/Default.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * デフォルトモジュール共通で使用するデータホルダー
+ *
+ * LICENSE: ライセンスに関する情報
+ *
+ * @category   Setuco
+ * @package    Setuco_Data
+ * @subpackage Constant_Module
+ * @license    http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
+ * @copyright  Copyright (c) 2010 SetucoCMS Project.(http://sourceforge.jp/projects/setucocms)
+ * @link
+ * @version
+ * @since      File available since Release 0.1.0
+ * @author     suzuki-mar
+ */
+
+/**
+ * @package    Setuco_Data
+ * @subpackage Constant
+ * @author     suzuki-mar
+ */
+class Setuco_Data_Constant_Module_Default
+{
+    /**
+     * 新着記事表示用に標準で何件取得するか
+     */
+    const LIMIT_GET_NEW_PAGE = 10;
+
+}
