6cc751d5bf34c1f0e500b46ab2a6579deef51192
charlesvineyard <charles.vineyard@gmail.com>
2010-12-15 18:31:38 +0900

    更新目標のフォームで0のとき「ページ」が消えるバグを修正

--- library/Setuco/Form/Decorator/SuffixString.php
+++ library/Setuco/Form/Decorator/SuffixString.php
@@ -90,38 +90,21 @@ class Setuco_Form_Decorator_SuffixString extends Zend_Form_Decorator_Abstract
     {
         $element = $this->getElement();
         $view    = $element->getView();
+
         if (null === $view) {
             return $content;
         }
 
-        $value = $element->getValue();
-        $value = trim($value);
+        $value     = $this->getValue();
 
-        if (!empty($value) && (null !== ($translator = $element->getTranslator()))) {
+        if (null !== ($translator = $element->getTranslator())) {
             $value = $translator->translate($value);
         }
 
-        if (empty($value)) {
-            return $content;
-        }
-
-        $separator = $this->getSeparator();
-        $placement = $this->getPlacement();
-        $value     = $this->getValue();
-        $escape    = $this->getEscape();
-
-        $options   = $this->getOptions();
-
-        if ($escape) {
+        if ($this->getEscape()) {
             $value = $view->escape($value);
         }
 
-        switch ($placement) {
-            case self::PREPEND:
-                return $value . $separator . $content;
-            case self::APPEND:
-            default:
-                return $content . $separator . $value;
-        }
+        return $content . $value . $this->getSeparator();
     }
 }
