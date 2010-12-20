9e24c619c81651232707ca21e0cb8dc06f5e46c6
charlesvineyard <charles.vineyard@gmail.com>
2010-12-20 16:14:03 +0900

    トップページのエラメッセージのパーシャル化とescape
    野望が更新できなくなってたので修正。

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -153,13 +153,13 @@ class Admin_IndexController extends Setuco_Controller_Action_AdminAbstract
     public function updateAmbitionAction()
     {
         $form = $this->_createAmbitionForm();
-        $ambition = $form->getValue('ambition');
+        $preAmbition = $form->getValue('ambition');
         if (!$form->isValid($_POST)) {
-            $form->getElement('ambition')->setValue($ambition);
+            $form->getElement('ambition')->setValue($preAmbition);
             $this->_setParam('ambitionForm', $form);
             return $this->_forward('index');
         }
-        $this->_ambitionService->updateAmbition($ambition);
+        $this->_ambitionService->updateAmbition($form->getValue('ambition'));
         $this->_helper->redirector('index');
     }
 
