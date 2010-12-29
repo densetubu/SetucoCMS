fb22251c01a75ce5653b8f79119d683722247a67
charlesvineyard <charles.vineyard@gmail.com>
2010-12-29 15:34:57 +0900

    フラッシュメッセージとエラーメッセージが消えなくなってたので修正
    JSの関数名をhideMessageAreaに変更。
    JSの修正。

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,10 +4,8 @@ dojo.require('dojo.NodeList-html');
 /********************
  * global functions
  ********************/
-function hideFlashMessage() {
-    dojo.style('messageArea', {
-        display: 'none'
-    });
+function hideMessageArea() {
+    dojo.query('.messageArea').style('display', 'none');
 }
 function confirmDeleteRedirect(url, item){
     if (confirm("「" + item + "」を本当に削除してよろしいですか？")) {
