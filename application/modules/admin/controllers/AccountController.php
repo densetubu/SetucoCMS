bde2e87169ca69c602b0054d4f43c489f37bc2d9
ErinaMikami <erina.m29@gmail.com>
2011-01-23 17:08:58 +0900

    アカウントページを作成
    アカウントパスワードを変更できるようにするため、アカウントページのphtmlを貼りつけ。

---
+++ application/modules/admin/controllers/AccountController.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * 管理側のアカウント情報編集のコントローラーです。
+ *
+ * LICENSE: ライセンスに関する情報
+ *
+ * @category 	Setuco
+ * @package 	Admin
+ * @subpackage  Controller
+ * @copyright   Copyright (c) 2010 SetucoCMS Project.
+ * @license
+ * @version
+ * @link
+ * @since       File available since Release 0.1.0
+ * @author      suzuki-mar
+ */
+
+/**
+ * @category    Setuco
+ * @package     Admin
+ * @subpackage  Controller
+ * @copyright   Copyright (c) 2010 SetucoCMS Project.
+ * @license
+ * @author      suzuki-mar
+ */
+class Admin_AccountController extends Setuco_Controller_Action_AdminAbstract
+{
+
+    /**
+     * アカウント情報を表示するアクションです
+     *
+     * @return void
+     * @author suzuki-mar
+     */
+    public function indexAction()
+    {
+    }
+}
