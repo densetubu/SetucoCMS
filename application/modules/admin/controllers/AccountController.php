220467f49193bcba50266d8aab755b5cd6f80c29
suzuki_mar <supasu145@gmail.com>
2011-02-02 18:07:28 +0900

    マイアカウントの編集のパスワードのバリデートエラーの文言の修正

--- application/modules/admin/controllers/AccountController.php
+++ application/modules/admin/controllers/AccountController.php
@@ -136,7 +136,7 @@ class Admin_AccountController extends Setuco_Controller_Action_AdminAbstract
         $passValidators[] = $confirmCheck;
 
         $passwordCheck = new Setuco_Validate_Password();
-        $passwordCheck->setMessage('パスワードに使用できる文字は半角英数字とアンダースコア、バックスラッシュを除く半角記号です。');
+        $passwordCheck->setMessage('パスワードに使用できる文字は半角英数字とアンダースコアとバックスラッシュを除いた一部の半角記号のみです。');
         $passValidators[] = $passwordCheck;
 
         $passElement->addValidators($passValidators);
