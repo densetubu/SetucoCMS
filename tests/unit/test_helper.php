36c846a1760356c5492e94ed45f2bc5409cf81de
akitsukada <tsukada.akihiro@gmail.com>
2011-09-11 13:26:33 +0900

    use DIRECTORY_SEPARATOR in test_helper.php

--- tests/unit/test_helper.php
+++ tests/unit/test_helper.php
@@ -1,23 +1,23 @@
 <?php
-
+$ds = DIRECTORY_SEPARATOR;
 // Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../../application'));
+defined("APPLICATION_PATH")
+    || define("APPLICATION_PATH", realpath(dirname(__FILE__) . "${ds}..${ds}..${ds}application"));
 
 // Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'testing'));
+defined("APPLICATION_ENV")
+    || define("APPLICATION_ENV", (getenv("APPLICATION_ENV") ? getenv("APPLICATION_ENV") : "testing"));
 
-define('APPLICATION_TEST_BOOTSTRAP_PATH', realpath(dirname(__FILE__) . '/application/bootstrap.php'));
-define('LIBRARY_TEST_BOOTSTRAP_PATH', realpath(dirname(__FILE__) . '/library/bootstrap.php'));
+define("APPLICATION_TEST_BOOTSTRAP_PATH", realpath(dirname(__FILE__) . "${ds}application${ds}bootstrap.php"));
+define("LIBRARY_TEST_BOOTSTRAP_PATH", realpath(dirname(__FILE__) . "${ds}library${ds}bootstrap.php"));
 
 set_include_path(
-     APPLICATION_PATH . '/../library' . PATH_SEPARATOR .
-     APPLICATION_PATH . '/controllers' . PATH_SEPARATOR .
-     realpath(dirname(__FILE__) . '/testrunner') . PATH_SEPARATOR .
+     APPLICATION_PATH . "${ds}..${ds}library" . PATH_SEPARATOR .
+     APPLICATION_PATH . "${ds}controllers" . PATH_SEPARATOR .
+     realpath(dirname(__FILE__) . "${ds}testrunner") . PATH_SEPARATOR .
      get_include_path()
 );
 
-require_once 'Zend/Loader/Autoloader.php';
+require_once "Zend${ds}Loader${ds}Autoloader.php";
 $autoloader = Zend_Loader_Autoloader::getInstance();
 $autoloader->setFallbackAutoloader(TRUE);
