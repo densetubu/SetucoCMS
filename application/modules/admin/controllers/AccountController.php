bb695aedb7e66d417bdaa5d47f5ed2fd17722409
Kyouhei Karino <hope637@gmail.com>
2012-03-05 02:16:11 +0900

    アカウント作成フォームにアカウントIDを半角英数しか受け付けないようバリデーションを追加

--- application/modules/admin/controllers/AccountController.php
+++ application/modules/admin/controllers/AccountController.php
@@ -244,6 +244,10 @@ class Admin_AccountController extends Setuco_Controller_Action_AdminAbstract
         $noRecordExists->setMessage( '「%value%」は既に登録されています。' );
         $validators[] = array($noRecordExists, true);
 
+		$stringAlpha = new Zend_Validate_Alpha();
+		$stringAlpha->setMessage('アカウントIDは半角英数で入力してください');
+        $validators[] = array($stringAlpha, true);
+
         return $validators;
     }
 
