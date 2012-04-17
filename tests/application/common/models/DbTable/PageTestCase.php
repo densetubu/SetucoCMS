94cb574263853a4b6ec038bdf32ef64f8f91c3ca
suzuki_mar <supasu145@gmail.com>
2012-04-17 20:28:04 +0900

    Zend_Test_PHPUnit_DatabaseTestを継承したSetuco_Test_PHPUnit_DatabaseTestCaseを作成した
    データ追加処理などの基本処理は、Setuco_Test_PHPUnit_DatabaseTestCaseに記述する

--- tests/application/common/models/DbTable/PageTestCase.php
+++ tests/application/common/models/DbTable/PageTestCase.php
@@ -7,15 +7,21 @@
 require_once '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
 
-class PageTestCase extends PHPUnit_Framework_TestCase
+class PageTestCase extends Setuco_Test_PHPUnit_DatabaseTestCase
 {
+
 
     public function setup()
     {
-        $this->_dao = new Common_Model_DbTable_Page();
+        parent::setUp();
+
+        $this->_dao = new Common_Model_DbTable_Account($this->getAdapter());
     }
 
-    
+    public function test_first()
+    {
+        $this->assertDataSetsEqual($this->getDataSet('account'), $this->getConnection()->createDataSet(array('account')));
+    }
 
 }
 
