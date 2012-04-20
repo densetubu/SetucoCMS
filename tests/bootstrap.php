4c130412049e4f0fd8f0aee06a265cf353fec55f
suzuki_mar <supasu145@gmail.com>
2012-04-20 11:44:11 +0900

    Tagサービスクラスのテストケースを作成した
    TagサービスのDBAdapterをテスト環境のものも使用できるようにした

---
+++ tests/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+define('ROOT_DIR', '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..');
+define('LIB_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'library' . DIRECTORY_SEPARATOR);
+define('APPLICATION_PATH', ROOT_DIR . DIRECTORY_SEPARATOR . 'application');
+
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(LIB_DIR),
+    get_include_path(),
+)));
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    'testing',
+    APPLICATION_PATH . '/configs/application.ini'
+);
+
+$application->bootstrap();
+
+
