04fbe0e8b938fdfa8a96cde12ac2a023cf7d9e65
suzuki_mar <supasu145@gmail.com>
2012-04-20 14:07:36 +0900

    開発を補助するためのページのモジュールを作成した
    とりあえずは、テストデータを入れるアクションを作成する

---
+++ application/modules/dev/controllers/IndexController.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * インストーラのコントローラ
+ *
+ * LICENSE: ライセンスに関する情報
+ *
+ * @category   Setuco
+ * @package    Install
+ * @subpackage Controller
+ * @license    http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
+ * @copyright  Copyright (c) 2010 SetucoCMS Project.(http://sourceforge.jp/projects/setucocms)
+ * @link
+ * @since      File available since Release 0.1.0
+ * @author     suzuki-mar
+ * @todo       フォーム処理をZenfFormに置き換える
+ */
+
+/**
+ * Description of InstallController
+ *
+ * @author suzuki-mar
+ */
+class Dev_IndexController
+    extends Setuco_Controller_Action_Abstract
+{
+
+    /**
+     * アクションの共通設定
+     * @author suzuki-mar
+     */
+    function init()
+    {
+        parent::init();
+    }
+
+    /**
+     * リンク画面
+     *
+     * @author suzuki-mar
+     * @return void
+     */
+    public function indexAction()
+    {
+
+    }
+
+    public function dbTestDataInsertAction()
+    {
+        
+    }
+
+}
