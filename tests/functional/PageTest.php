66c2795e9b31a5f1b97b72b1a376e753f5c2887e
suzuki_mar <supasu145@gmail.com>
2012-05-16 19:44:33 +0900

    ページ検索の機能テストを途中まで作成した

--- tests/functional/PageTest.php
+++ tests/functional/PageTest.php
@@ -12,31 +12,68 @@ class PageTest extends Setuco_Test_PHPUnit_SeleniumTestCase
         $this->type("id=query", "タイトル 検索して");
         $this->click("id=sub_search");
         $this->waitForPageToLoad("30000");
-        $this->assertTrue($this->isTextPresent("タイトルで検索して"));
+        $this->assertTrue($this->isTextPresent(Fixture_Page::TITLE_TITLE));
+
+        $this->assertCountItemList(1);
     }
 
-    public function test_概要をAND条件で検索できるか()
+    public function test_アウトラインをAND条件で検索できるか()
     {
         $this->_login();
         $this->open("/admin/page");
 
-        $this->type("id=query", "アウトライン 検索して");
+        $this->type("id=query", "アウトライン");
         $this->click("id=sub_search");
         $this->waitForPageToLoad("30000");
-        $this->assertTrue($this->isTextPresent("アウトラインで検索して"));
+        $this->assertTrue($this->isTextPresent(Fixture_Page::OUTLINE_TITLE));
+
+        $this->assertCountItemList(1);
     }
 
     public function test_コンテンツをAND条件で検索できるか()
     {
         $this->open("/admin/page");
-        $this->type("id=query", "コンテンツ");
+        $this->type("id=query", "コンテンツ　検索");
         $this->click("id=sub_search");
         $this->waitForPageToLoad("30000");
-        $this->assertTrue($this->isTextPresent("コンテンツで検索して"));
+        $this->assertTrue($this->isTextPresent(Fixture_Page::CONTENTS_TITLE));
+
+        $this->assertCountItemList(1);
     }
 
+    public function test_タグをAND条件で検索する()
+    {
+        $this->open("/admin/page");
+        $this->type("id=query", "setuco test");
+        $this->click("id=sub_search");
+        $this->waitForPageToLoad("30000");
+        $this->assertTrue($this->isTextPresent(Fixture_Page::TAG_TITLE));
 
+        $this->assertCountItemList(1);
+    }
 
+    /**
+     * @group now
+     */
+    public function test_全ての項目から検索する()
+    {
+        $this->open("/admin/page");
+        $this->type("id=query", "検索して");
+        $this->click("id=sub_search");
+        $this->waitForPageToLoad("30000");
+
+        $titles = array(
+            Fixture_Page::TITLE_TITLE,
+            Fixture_Page::CONTENTS_TITLE,
+            Fixture_Page::OUTLINE_TITLE,
+            Fixture_Page::ACCOUNT_TITLE,
+            Fixture_Page::ACCOUNT_ONLY_TITLE,
+        );
+
+        $this->assertCountItemList(5);
+    }
+
+    
 
 }
 
