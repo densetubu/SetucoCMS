11c14f5ff1c970003ff4cb85a862977dc7515732
aki.star02 <aki.star02@gmail.com>
2012-05-26 23:21:01 +0900

    エディタで挿入できる絵文字の数を増やしました（altが調整できてないです)
    Conflicts:
    
    	application/modules/admin/views/layouts/layout.phtml
    	public/js/tinymce_js/lists/image_list.js
    	public/js/tinymce_js/plugins/advimage/image.htm
    	public/js/tinymce_js/plugins/emotions/emotions.htm

--- public/js/tinymce_js/lists/image_list.js
+++ public/js/tinymce_js/lists/image_list.js
@@ -1,6 +1,11 @@
 // This list may be created by a server logic page PHP/ASP/ASPX/JSP in some backend system.
 // There images will be displayed as a dropdown in all image dialogs if the "external_link_image_url"
 // option is defined in TinyMCE init.
+//
 
-var mediaList = new ApiDataList("/api/media/image");
-var tinyMCEImageList = mediaList.getTinyMCEListDataByMapList(["name", "uploadUrl"]);
+var tinyMCEImageList = new Array(
+	// Name, URL
+	["Logo 1", "/js/tinymce_js/media/logo.jpg"],
+	["Logo 2", "/js/tinymce_js/media/logo.jpg"]
+);
+
--- public/js/tinymce_js/lists/media_list.js
+++ public/js/tinymce_js/lists/media_list.js
@@ -2,5 +2,9 @@
 // There flash movies will be displayed as a dropdown in all media dialog if the "media_external_list_url"
 // option is defined in TinyMCE init.
 
-var mediaList = new ApiDataList("/api/media/etc");
-var tinyMCEMediaList = mediaList.getTinyMCEListDataByMapList(["name", "uploadUrl"]);
+var tinyMCEMediaList = new Array(
+	// Name, URL
+	["Logo 1", "/js/tinymce_js/media/logo.jpg"],
+	["Logo 2", "/js/tinymce_js/media/logo.jpg"]
+);
+
