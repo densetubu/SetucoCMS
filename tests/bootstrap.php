f719090b28506be065bab95e686a2a00f3fe0c3a
suzuki_mar <supasu145@gmail.com>
2012-05-30 13:06:11 +0900

    testのbootstrapをリファクタリングした
    同じ定数が2回定義されてしまうのを防いだ

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,24 @@
 <?php
 
-define('ROOT_DIR', substr(preg_replace("/tests.*$/", '', __DIR__), 0, -1));
-define('LIB_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'library' . DIRECTORY_SEPARATOR);
-define('APPLICATION_PATH', ROOT_DIR . DIRECTORY_SEPARATOR . 'application');
-define('TEST_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR);
-define('BOOT_STRAP_FINSHED', true);
+if (!defined('ROOT_DIR')) {
+    define('ROOT_DIR', substr(preg_replace("/tests.*$/", '', __DIR__), 0, -1));
+}
+
+if (!defined('LIB_DIR')) {
+    define('LIB_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'library' . DIRECTORY_SEPARATOR);
+}
+
+if (!defined('APPLICATION_PATH')) {
+    define('APPLICATION_PATH', ROOT_DIR . DIRECTORY_SEPARATOR . 'application');
+}
+
+if (!defined('TEST_DIR')) {
+    define('TEST_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR);
+}
+
+if (!defined('BOOT_STRAP_FINSHED')) {
+    define('BOOT_STRAP_FINSHED', true);
+}
 
 
 // Ensure library/ is on include_path
