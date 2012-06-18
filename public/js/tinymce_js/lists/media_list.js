daf8dd18c4a72f729d8fc11efe96b298276ac056
aki.star02 <aki.star02@gmail.com>
2012-06-19 01:04:22 +0900

    画像をエディタから挿入できるようにした（バグあり

--- public/js/tinymce_js/lists/media_list.js
+++ public/js/tinymce_js/lists/media_list.js
@@ -2,9 +2,5 @@
 // There flash movies will be displayed as a dropdown in all media dialog if the "media_external_list_url"
 // option is defined in TinyMCE init.
 
-var tinyMCEMediaList = new Array(
-	// Name, URL
-	["Logo 1", "/js/tinymce_js/media/logo.jpg"],
-	["Logo 2", "/js/tinymce_js/media/logo.jpg"]
-);
-
+var mediaList = new ApiDataList("/api/media/etc");
+var tinyMCEMediaList = mediaList.getTinyMCEListDataByMapList(["name", "uploadUrl"]);
--- public/js/tinymce_js/lists/template_list.js
+++ public/js/tinymce_js/lists/template_list.js
@@ -2,9 +2,5 @@
 // There templates will be displayed as a dropdown in all media dialog if the "template_external_list_url"
 // option is defined in TinyMCE init.
 
-var tinyMCETemplateList = new Array(
-	// Name, URL
-	["Logo 1", "/js/tinymce_js/media/logo.jpg"],
-	["Logo 2", "/js/tinymce_js/media/logo.jpg"]
-);
-
+var templateList = new ApiDataList("/api/template");
+var tinyMCETemplateList = templateList.getTinyMCEListDataByMapList(["title", "url", "explanation"]);
