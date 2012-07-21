7ba142a52fbed6c574c10a595ee0a336e0edd6c8
Takayuki Otake <k.wisiiy@gmail.com>
2012-07-21 17:34:54 +0900

    多重送信防止で無効化したボタンを再度有効化させないようにした
    一定時間で有効化させたら、ページの読み込みが遅い場合などにまたボタンが有効化されてしまってボタンが押されてしまう可能性がある
    もしページ遷移しないボタンなどで再び有効化したい時は、それを利用するタイミングで個別に有効化すればいい

--- public/js/form.js
+++ public/js/form.js
@@ -1,14 +1,8 @@
 
 $(function() {
     //this event from http://www.alink.co.jp/tech/blog/2009/04/05/javascript-jquery%E3%81%A7%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A0%E3%81%AE2%E9%87%8D%E9%80%81%E4%BF%A1%E3%82%92%E9%98%B2%E3%81%90/
-    $.fn.disableDoubleSubmit = function(timeout) {
-        if(!timeout || timeout < 1) {
-            timeout = 3000; // 3sec.
-        }
+    $.fn.disableDoubleSubmit = function() {
         $(this).bind("submit",function() {
-            var submit_buttons = $(":submit, :image",this);
-            submit_buttons.attr("disabled", true);
-
             //ページ内に存在する全てのボタンを無効化させる
             $('button').each(function (i) {
                 $(this).attr("disabled", true);
@@ -19,15 +13,12 @@ $(function() {
                 }
             });
 
-            setTimeout(function(){
-                submit_buttons.attr("disabled", false);
-            },timeout);
         });
         return this;
     };
 
 
-    $("form").disableDoubleSubmit(1000);
+    $("form").disableDoubleSubmit();
 
 });
 
