71b7f994166a1bb750421bc12edc7df8c4adebae
Takayuki Otake <k.wisiiy@gmail.com>
2012-07-28 10:06:42 +0900

    一部ページで多重送信防止が働かない不具合修正

--- public/js/setuco/form.js
+++ public/js/setuco/form.js
@@ -1,6 +1,8 @@
 
-$(function() {
-    //this event from http://www.alink.co.jp/tech/blog/2009/04/05/javascript-jquery%E3%81%A7%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A0%E3%81%AE2%E9%87%8D%E9%80%81%E4%BF%A1%E3%82%92%E9%98%B2%E3%81%90/
+//todo: setTimeoutでてきとーな時間遅らせてイベントセットするんじゃなくてDOM構築完了時点で読み込ませるようにする
+setTimeout(function() {
+
+    //多重送信防止
     $.fn.disableDoubleSubmit = function() {
         $(this).bind("submit",function() {
             //ページ内に存在する全てのボタンを無効化させる
@@ -20,5 +22,5 @@ $(function() {
 
     $("form").disableDoubleSubmit();
 
-});
+}, 500);
 
