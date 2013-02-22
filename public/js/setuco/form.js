e47135aabb7e6d578e36303d2245f6c6a0b026a0
Takayuki Otake <k.wisiiy@gmail.com>
2013-02-23 00:05:51 +0900

    ページ編集時に「公開して保存」が下書き保存されてしまうバグの修正

--- public/js/setuco/form.js
+++ public/js/setuco/form.js
@@ -4,6 +4,13 @@ setTimeout(function() {
 
     //多重送信防止
     $.fn.disableDoubleSubmit = function() {
+        //ボタン内のname,valueを送信
+        $('input[type=submit],input[type=button],button').click(function(){
+            var name = $(this).attr('name');
+            var value = $(this).attr('value');
+            $(this).parent().append('<input type="hidden" name="'+ name +'" value="'+ value +'">');
+        });
+
         $(this).bind("submit",function() {
             //ページ内に存在する全てのボタンを無効化させる
             $('button').each(function (i) {
