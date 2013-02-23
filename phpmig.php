b43f8ed6cc033c5d1827c6b0993c5d81c88c06da
Takayuki Otake <k.wisiiy@gmail.com>
2013-02-24 02:35:10 +0900

    マイグレーションツールを内包

---
+++ phpmig.php
@@ -0,0 +1,64 @@
+<?php
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+//Define SetucoCMS version
+defined('APPLICATION_VERSION') || define('APPLICATION_VERSION', '0.1.0');    
+    
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+/**
+ * デバッグ用関数
+ * 
+ * @param mixed $var デバッグしたい変数
+ * @param string $label デバッグ表示の際のラベル
+ */
+function d($var, $label = null) {
+    Zend_Debug::dump($var, $label);
+}
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+
+
+
+// Create application, bootstrap, and run
+$app_ini = APPLICATION_PATH . '/configs/application.ini';
+if (!file_exists($app_ini)) {
+    echo "Error\n";
+    echo "Configuration file not exists: '$app_ini'\n";
+    exit;
+}
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    $app_ini
+);
+
+use \Phpmig\Adapter,
+    \Phpmig\Pimple\Pimple;
+require_once 'Setuco/Db/ConnectionFactory.php';
+
+$container = new Pimple();
+
+$container['app'] = $application;
+$container['db'] = Setuco_Db_ConnectionFactory::create(APPLICATION_ENV);
+$container['phpmig.adapter'] = $container->share(function () {
+    // replace this with a better Phpmig\Adapter\AdapterInterface 
+    return new Adapter\File\Flat(__DIR__ . DIRECTORY_SEPARATOR . 'migrations/.migrations.log');
+});
+
+$container['phpmig.migrations'] = function() {
+    return glob(__DIR__ . DIRECTORY_SEPARATOR . 'migrations/*.php');
+};
+
+return $container;
